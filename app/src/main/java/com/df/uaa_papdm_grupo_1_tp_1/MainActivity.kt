50f57456b03594a9e8ee7199cf438e4ee2532098
gabrielgtl <gabriel.sanove@gmail.com>
2024-10-05 15:34:35 -0400

    Función ListaDeProductos que invoca a la funcion TarjetaDeProducto por cada producto existente. Función TarjetaDeProductos, crear las tarjetas que contendran el nombre del producto y la información (información creada con la función InfoProducto a crear más tarde).

--- app/src/main/java/com/df/uaa_papdm_grupo_1_tp_1/MainActivity.kt
+++ app/src/main/java/com/df/uaa_papdm_grupo_1_tp_1/MainActivity.kt
@@ -1,14 +1,23 @@
-package com.df.uaa_papdm_grupo_1_tp_1
+    package com.df.uaa_papdm_grupo_1_tp_1
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -112,3 +121,64 @@ fun FormularioDeProducto(
         }
     }
 }
+    @Composable
+    fun ListaDeProductos(productos: MutableList<Producto>) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(productos) { producto ->
+                TarjetaDeProducto(
+                    producto,
+                    onRemove = { productos.remove(producto) })
+            }
+        }
+    }
+
+    @Composable
+    fun TarjetaDeProducto(producto: Producto, onRemove: () -> Unit) {
+        Card(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp).fillMaxWidth()
+            ) {
+                Text(
+                    text = producto.nombre,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                BoxWithConstraints(
+                    modifier = Modifier.fillMaxWidth().height(150.dp)
+                ) {
+                    val anchoPantalla = maxWidth
+                    LazyRow(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        item { InfoProducto(titulo="Descripción", valor=producto.descripcion, ancho=anchoPantalla, isImage = false) }
+
+                        item { InfoProducto(titulo="Marca", valor=producto.marca, ancho=anchoPantalla, isImage = false) }
+
+                        item { InfoProducto(titulo="Precio", valor=producto.precio, ancho=anchoPantalla, isImage = false) }
+
+                        item { InfoProducto(titulo="Cantidad", valor=producto.cantidad, ancho=anchoPantalla, isImage = false) }
+
+                        item { InfoProducto(titulo="Imagen", valor=producto.imagenUrl, ancho=anchoPantalla, isImage = true) }
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Button(
+                        onClick = { onRemove() },
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text(text = "Eliminar")
+                    }
+                }
+
+            }
+        }
+    }
