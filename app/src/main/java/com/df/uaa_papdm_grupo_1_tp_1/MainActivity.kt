6936c6c16948e8e244b64193ec8db5edc9b538bd
UB3721 <aspiranteypatito@gmail.com>
2024-10-05 15:58:43 -0400

    Se agrego la libreria Coil para la utilización de AsyncImage. Se agregó función InfoProducto que se encarga de mostrar la información perteneciente al producto recibido.

--- app/src/main/java/com/df/uaa_papdm_grupo_1_tp_1/MainActivity.kt
+++ app/src/main/java/com/df/uaa_papdm_grupo_1_tp_1/MainActivity.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -24,8 +29,10 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.df.prueba.ui.theme.AppTheme
 
 data class Producto (
@@ -121,64 +128,102 @@ fun FormularioDeProducto(
         }
     }
 }
-    @Composable
-    fun ListaDeProductos(productos: MutableList<Producto>) {
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+
+@Composable
+fun ListaDeProductos(productos: MutableList<Producto>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(productos) { producto ->
+            TarjetaDeProducto(
+                producto,
+                onRemove = { productos.remove(producto) })
+        }
+    }
+}
+
+@Composable
+fun TarjetaDeProducto(producto: Producto, onRemove: () -> Unit) {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp).fillMaxWidth()
         ) {
-            items(productos) { producto ->
-                TarjetaDeProducto(
-                    producto,
-                    onRemove = { productos.remove(producto) })
+            Text(
+                text = producto.nombre,
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            BoxWithConstraints(
+                modifier = Modifier.fillMaxWidth().height(150.dp)
+            ) {
+                val anchoPantalla = maxWidth
+                LazyRow(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    item { InfoProducto(titulo="Descripción", valor=producto.descripcion, ancho=anchoPantalla, isImage = false) }
+
+                    item { InfoProducto(titulo="Marca", valor=producto.marca, ancho=anchoPantalla, isImage = false) }
+
+                    item { InfoProducto(titulo="Precio", valor=producto.precio, ancho=anchoPantalla, isImage = false) }
+
+                    item { InfoProducto(titulo="Cantidad", valor=producto.cantidad, ancho=anchoPantalla, isImage = false) }
+
+                    item { InfoProducto(titulo="Imagen", valor=producto.imagenUrl, ancho=anchoPantalla, isImage = true) }
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(
+                    onClick = { onRemove() },
+                    modifier = Modifier.wrapContentSize()
+                ) {
+                    Text(text = "Eliminar")
+                }
             }
         }
     }
+}
 
-    @Composable
-    fun TarjetaDeProducto(producto: Producto, onRemove: () -> Unit) {
-        Card(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Column(
-                modifier = Modifier.padding(16.dp).fillMaxWidth()
+@Composable
+fun InfoProducto(titulo: String, valor: String, ancho: androidx.compose.ui.unit.Dp, isImage: Boolean) {
+    Column(
+        modifier = Modifier
+            .width(ancho)
+            .fillMaxSize()
+            .padding(end = 32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = titulo,
+            style = MaterialTheme.typography.titleSmall
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        if (isImage) {
+            AsyncImage(
+                model = valor,
+                placeholder = painterResource(id = R.drawable.ic_placeholder),
+                error = painterResource(id = R.drawable.ic_error),
+                contentDescription = "Imagen Producto",
+                modifier = Modifier.fillMaxSize()
+            )
+        } else {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
             ) {
                 Text(
-                    text = producto.nombre,
-                    style = MaterialTheme.typography.headlineSmall
+                    text = valor,
+                    style = MaterialTheme.typography.titleLarge
                 )
-                Spacer(modifier = Modifier.height(8.dp))
-                BoxWithConstraints(
-                    modifier = Modifier.fillMaxWidth().height(150.dp)
-                ) {
-                    val anchoPantalla = maxWidth
-                    LazyRow(
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        item { InfoProducto(titulo="Descripción", valor=producto.descripcion, ancho=anchoPantalla, isImage = false) }
-
-                        item { InfoProducto(titulo="Marca", valor=producto.marca, ancho=anchoPantalla, isImage = false) }
-
-                        item { InfoProducto(titulo="Precio", valor=producto.precio, ancho=anchoPantalla, isImage = false) }
-
-                        item { InfoProducto(titulo="Cantidad", valor=producto.cantidad, ancho=anchoPantalla, isImage = false) }
-
-                        item { InfoProducto(titulo="Imagen", valor=producto.imagenUrl, ancho=anchoPantalla, isImage = true) }
-                    }
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
-                ) {
-                    Button(
-                        onClick = { onRemove() },
-                        modifier = Modifier.wrapContentSize()
-                    ) {
-                        Text(text = "Eliminar")
-                    }
-                }
-
             }
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
+}
